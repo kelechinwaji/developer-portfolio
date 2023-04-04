184d22e26ed115668da50b5dc38f08e62855b04e
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-04-04 08:08:21 +0100

    [feat]: socials

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,12 +5,12 @@ export default function Socials()
 {
     return (
         <nav>
-            <h1 className="logo">emmanuelMilord</h1>
+            <h1 className="logo">KelechukwuNwaji</h1>
             <div className="socials">
-                <a href="https://github.com/GuyNamedEJ" className="social-item"><FaGithub /></a>
-                <a href="https://www.frontendmentor.io/profile/GuyNamedEJ" className="social-item"><SiFrontendmentor /></a>
-                <a href="https://www.linkedin.com/in/emmanuel-milord/" className="social-item"><FaLinkedin /></a>
-                <a href="https://twitter.com/GuyNamedEJ" className="social-item"><FaTwitter /></a>
+                <a href="https://github.com/kelechinwaji" className="social-item"><FaGithub /></a>
+    
+                <a href="https://www.linkedin.com/in/kelechukwu-nwaji/" className="social-item"><FaLinkedin /></a>
+                <a href="https://twitter.com/kelechinwaji" className="social-item"><FaTwitter /></a>
             </div>
         </nav>
     )
